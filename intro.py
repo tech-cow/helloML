2b8efb4a08ba09b263612794b5dbccbf8939a15f
Yu Zhou <junior147147@yahoo.com>
2017-07-02 15:00:58 -0700

    shifting value to predict future closing price

--- intro.py
+++ intro.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import quandl
+import math
 
 '''
 Using quandl.get to get a pandas data frame
@@ -15,17 +16,40 @@ The parameter returns can take the following values:
 When returns is omitted, a pandas dataframe is returned.
 '''
 
-#dataframe
+# Initialize DataFrame contains all Feature of Google Stock
 df = quandl.get('WIKI/GOOGL')
 
-#Data Cleaning
+# Select needed features
 df = df[['Adj. Open','Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+df['Closing_Price'] = df['Adj. Close']
 
-# Hashing HL_PCT & PCT_change with our existing data.
+# Hashing new features
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+df = df[['Closing_Price', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
-# Generating new data
-df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+forecast_col = 'Closing_Price'
+# As later we are shifting Closing price, at the tail of the table, we won't be getting
+# Value that is ahead of its time, so we would need to fill those empty data with a value
+# Panda fillna, fill all of the N/A data with a value rather than getting rid of them
+df.fillna(-99999, inplace = True)
+
+# Math.ceil rounds off value to its nearest ceil
+# 1 percent of the days
+forecast_out = int(math.ceil(0.01*len(df)))
+
+# Setting label feature to predict its Closing price 1% days into the future
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+
+# Display
+
+print "\n"
+print "######################################################################"
+print "#    Display future closing price of a certain stock at given days   #"
+print "######################################################################"
+print "\n"
 
 print df.head()
+print "---------------------------------"
+print df.tail()
